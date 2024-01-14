f0bad44ac027eba83df8bf55a97ed483efd0a3e7
Romeq03 <romekbartek18@gmail.com>
2024-01-14 15:08:11 +0100

    new

---
+++ for2/petla_for.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Obliczanie wartości funkcji y = √(3^2) * x dla x od 0 do 10:");
+
+        for (int x = 0; x <= 10; x++)
+        {
+            double y = Math.Sqrt(3 * 3) * x;
+            Console.WriteLine($"Dla x = {x}, y = {y}");
+        }
+
+        Console.ReadLine(); // Zatrzymuje konsolę przed zamknięciem
+    }
+}
